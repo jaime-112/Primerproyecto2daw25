fa3292b0c19d2c32fb67cbabe1313b024abf1da1
jaime-112 <xjlundel035@ieshnosmachado.org>
2025-09-18 10:21:34 +0200

    Ejercicios clase

--- PRueba1.py
+++ PRueba1.py
@@ -1,5 +1,11 @@
-# Haz que inserte por pantalla 2 numeros y muestre la suma de estos
+# Haz que inserte por pantalla 2 numeros y muestre la suma, resta, multiplicación, división de estos
 num1 = int(input("Ingrese el primer número: "))
 num2 = int(input("Ingrese el segundo número: "))
 sumanumeros = num1 + num2
+restanumeros = num1 - num2
+multinumeros = num1 * num2
+divinumeros =float( num1 / num2)
 print(" La suma de estos números es de: ", sumanumeros)
+print(" La resta de estos números es de: ", restanumeros)
+print(" La multiplicación de estos números es de: ", multinumeros)
+print(" La división de estos números es de: ", divinumeros)
