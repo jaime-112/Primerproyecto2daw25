711955bb1d73852b16d6b7c8540ed8f76334316b
jaime-112 <xjlundel035@ieshnosmachado.org>
2025-09-18 09:50:36 +0200

    Primeros ejercicios

--- PRueba1.py
+++ PRueba1.py
@@ -1,1 +1,4 @@
-print("Hola bb")
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+sumanumeros = num1 + num2
+print(" La suma de estos números es de: ", sumanumeros)
